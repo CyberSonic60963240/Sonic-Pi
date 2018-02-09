4d77985bcc2ed5459a18c90fba95125fdfe03a8f
CyberSonic60963240 <Gregory_Cowgill@davincischools.org>
2018-02-09 14:46:26 -0800

    Add files via upload

---
+++ Piano.rb
@@ -0,0 +1,199 @@
+use_bpm 300
+x = 9
+1.times do
+  1.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  2.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  3.times do
+    play x
+    sleep 1
+    x = x + 2
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+  1.times do
+    play x
+    sleep 1
+    x = x + 1
+  end
+end
